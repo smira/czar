e4e66af575458411f7a75add25d294ed31320747
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2013-12-02 18:27:52 +0400

    Mock for server startup/shutdown.

--- raft/server.go
+++ raft/server.go
@@ -14,16 +14,22 @@ import (
 	"time"
 )
 
+type Server struct {
+	dbPath     string
+	raftServer raft.Server
+	transport  *ZmqTransporter
+}
+
 var logger = log.New(os.Stdout, "[raft-zeromq] ", log.Lmicroseconds)
 
 // Length of node name in bytes
 const nodeNameLength = 16
 
 // getNodeName loads or generates and saves node name
-func getNodeName(path string) (string, error) {
+func getNodeName(dbPath string) (string, error) {
 	var nodeName string
 
-	nodeNamePath := filepath.Join(path, "nodename")
+	nodeNamePath := filepath.Join(dbPath, "nodename")
 
 	// Figure out server name
 	buf, err := ioutil.ReadFile(nodeNamePath)
@@ -54,9 +60,56 @@ func getNodeName(path string) (string, error) {
 	return nodeName, nil
 }
 
-// NewRaftServer creates raft server with ØMQ transport
-func NewRaftServer(path string, listenAddress string, receiveTimeout time.Duration) (raft.Server, error) {
-	// var nodeName string
+// NewServer creates raft server with ØMQ transport
+func NewServer(dbPath string, listenAddress string, receiveTimeout time.Duration) (*Server, error) {
+	nodeName, err := getNodeName(dbPath)
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, nil
+	transporter, err := NewZmqTransporter(listenAddress, receiveTimeout)
+	if err != nil {
+		return nil, fmt.Errorf("error creating ZMQ transport: %v", err)
+	}
+
+	// TODO: stateMachine
+	server, err := raft.NewServer(nodeName, dbPath, transporter, nil, nil, listenAddress)
+	if err != nil {
+		return nil, fmt.Errorf("error creating Raft server: %v", err)
+	}
+
+	return &Server{dbPath: dbPath,
+		raftServer: server,
+		transport:  transporter,
+	}, nil
+}
+
+// Start makes raft server & ØMQ transport running, joins existing
+// leader or runs on its own
+func (s *Server) Start(leader string) error {
+	s.transport.Start(s.raftServer)
+	err := s.raftServer.Start()
+	if err != nil {
+		return fmt.Errorf("unable to start Raft server: %v", err)
+	}
+
+	if leader != "" {
+		// Attempting to join
+		if !s.raftServer.IsLogEmpty() {
+			return fmt.Errorf("cannot join with non-empty log")
+		}
+		// TODO: send Join command to leader
+	} else if s.raftServer.IsLogEmpty() {
+		// TODO: send Join command to myself
+	} else {
+		// Recovered from log
+		logger.Printf("Recovered from log\n")
+	}
+	return nil
+}
+
+// Stop shuts down raft server and transport
+func (s *Server) Stop() {
+	s.raftServer.Stop()
+	s.transport.Shutdown()
 }
